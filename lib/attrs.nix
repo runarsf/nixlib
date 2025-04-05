10eccb8bcc34a6d4bdf45dd0e8bbcc1d6b2e7a62
Runar Fredagsvik <i@runar.ch>
2025-04-06 00:58:18 +0200

    feat: add `fill`, `enable`, and `disable` functions

---
+++ lib/attrs.nix
@@ -0,0 +1,56 @@
+{ lib, lib', ... }:
+let
+  inherit (builtins) isString isList;
+
+  inherit (lib) foldl setAttrByPath recursiveUpdate;
+
+  inherit (lib') fmatch;
+in
+rec {
+  /**
+    Set the value of each attribute given a list of paths.
+
+    # Arguments
+
+    value
+    : The value to set.
+
+    xs
+    : A list of paths to set the value for.
+    : The paths can be strings or lists of strings.
+
+    # Example
+
+    ```nix
+    fill { enable = true; } [ "a" [ "a" "b" ] ]
+    => {
+      a = {
+        b = { enable = true; };
+        enable = true;
+      };
+    }
+    ```
+  */
+  fill =
+    value: xs:
+    let
+      normalizePath =
+        p:
+        fmatch p [
+          [
+            isList
+            p
+          ]
+          [
+            isString
+            [ p ]
+          ]
+        ];
+      attrsets = map (p: setAttrByPath (normalizePath p) value) xs;
+    in
+    foldl recursiveUpdate { } attrsets;
+
+  enable = fill { enable = true; };
+
+  disable = fill { enable = false; };
+}
