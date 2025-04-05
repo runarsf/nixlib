fdedc79dd9e4ba0b6183cdcf6a1ced670819f2d0
Runar Fredagsvik <i@runar.ch>
2025-04-06 00:56:14 +0200

    feat: add `mkModuleWithOptions` function

---
+++ lib/modules.nix
@@ -0,0 +1,65 @@
+{
+  lib,
+  lib',
+  ...
+}:
+let
+  inherit (builtins) isString isList throw;
+
+  inherit (lib)
+    mkOption
+    setAttrByPath
+    getAttrFromPath
+    mkIf
+    ;
+
+  inherit (lib.types) bool;
+
+  inherit (lib') fmatch;
+in
+{
+  mkModuleWithOptions =
+    {
+      config,
+      name,
+      moduleConfig,
+      default ? false,
+      extraCondition ? (x: x && true),
+    }:
+    let
+      options = moduleConfig.options or { };
+      options' = moduleConfig.options' or { };
+      cfg =
+        if (moduleConfig ? options || moduleConfig ? options') then
+          moduleConfig.config or (throw "Missing toplevel config attribute")
+        else
+          moduleConfig.config or moduleConfig;
+
+      pathList = fmatch name [
+        [
+          isList
+          name
+        ]
+        [
+          isString
+          [ name ]
+        ]
+      ];
+
+      modulePath = [ "modules" ] ++ pathList;
+      enableOptionPath = modulePath ++ [ "enable" ];
+
+      moduleOptions = {
+        enable = mkOption {
+          inherit default;
+          type = bool;
+          description = "Enable ${name} module";
+        };
+      } // options;
+    in
+    {
+      options = (setAttrByPath modulePath moduleOptions) // options';
+
+      config = mkIf (extraCondition (getAttrFromPath enableOptionPath config)) cfg;
+    };
+}
