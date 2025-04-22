d577aa0345b9196aefc6b1108eecc2e0688a188f
Runar Fredagsvik <i@runar.ch>
2025-04-22 14:57:31 +0200

    feat: add `fill'` function

--- src/attrs.nix
+++ src/attrs.nix
@@ -1,59 +1,76 @@
 {
   lib,
   lib',
-}: let
+}:
+let
   inherit (builtins) isString isList;
 
-  inherit (lib) foldl setAttrByPath recursiveUpdate;
+  inherit (lib.lists) foldl;
+
+  inherit (lib.attrsets) setAttrByPath recursiveUpdate;
+
+  inherit (lib.strings) splitString;
 
   inherit (lib'.matching) fmatch;
-in rec {
+in
+rec {
   exports = {
-    inherit fill enable disable;
+    inherit
+      fill
+      fill'
+      enable
+      disable
+      ;
   };
 
   /**
-  Set the value of each attribute given a list of paths.
+    Set the value of each attribute given a list of paths.
 
-  # Arguments
+    # Arguments
 
-  value
-  : The value to set.
+    value
+    : The value to set.
 
-  xs
-  : A list of paths to set the value for.
-  : The paths can be strings or lists of strings.
+    xs
+    : A list of paths to set the value for.
+    : The paths can be strings or lists of strings.
 
-  # Example
+    # Example
 
-  ```nix
-  fill { enable = true; } [ "a" [ "a" "b" ] ]
-  => {
-    a = {
-      b = { enable = true; };
-      enable = true;
-    };
-  }
-  ```
+    ```nix
+    fill { enable = true; } [ "a" [ "a" "b" ] ]
+    => {
+      a = {
+        b = { enable = true; };
+        enable = true;
+      };
+    }
+    ```
   */
 
-  fill = value: xs: let
-    normalizePath = p:
-      fmatch p [
-        [
-          isList
-          p
-        ]
-        [
-          isString
-          [p]
-        ]
-      ];
-    attrsets = map (p: setAttrByPath (normalizePath p) value) xs;
-  in
-    foldl recursiveUpdate {} attrsets;
+  fill =
+    value: xs:
+    let
+      normalizePath =
+        p:
+        fmatch p [
+          [
+            isList
+            p
+          ]
+          [
+            isString
+            [ p ]
+          ]
+        ];
+      attrsets = map (p: setAttrByPath (normalizePath p) value) xs;
+    in
+    foldl recursiveUpdate { } attrsets;
 
-  enable = fill {enable = true;};
+  # The same as fill, but splits the paths by '.'
+  fill' = value: xs: fill value <| map (x: splitString "." x) xs;
 
-  disable = fill {enable = false;};
+  enable = fill { enable = true; };
+
+  disable = fill { enable = false; };
 }
