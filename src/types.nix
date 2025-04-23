6848e64b171630f30d99202ecd3ccfd6dd3f5de4
Runar Fredagsvik <i@runar.ch>
2025-04-23 11:51:00 +0200

    feat: add deep-mergeable `types.attrs.merge` override

--- src/types.nix
+++ src/types.nix
@@ -1,6 +1,20 @@
-{lib}: let
+{
+  lib,
+  lib',
+}: let
+  inherit (builtins) map;
+
+  inherit (lib') deepMerge;
+
   inherit (lib.types) mkOptionType;
 in {
+  # Override nixpkgs.lib.types.attrs to be deep-mergeable. This avoids configs
+  # from mistakenly overriding values due to the use of `//`.
+  attrs.merge = _: definitions: let
+    values = map (definition: definition.value) definitions;
+  in
+    deepMerge values;
+
   anything = mkOptionType {
     name = "anything";
     description = "Anything!";
