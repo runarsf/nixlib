27a2484e5e1ca9e87a8c0c3fb71535e0d5eb3dcd
Runar Fredagsvik <i@runar.ch>
2025-07-10 12:34:39 +0200

    feat: add `mkIfElse` function

---
+++ src/conditions.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+}:
+let
+  inherit (lib.modules) mkIf mkMerge;
+in
+rec {
+  exports = {
+    inherit
+      mkIfElse
+      ;
+  };
+
+  mkIfElse =
+    p: yes: no:
+    mkMerge [
+      (mkIf p yes)
+      (mkIf (!p) no)
+    ];
+}
